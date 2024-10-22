ffb582770807a074ffa2d8b51d77dae6168eb146
unknown <chicksss>
2024-10-23 07:01:13 +0800

    first commit

---
+++ scipt.js
@@ -0,0 +1,18 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// // gsap.to('.y1',{
+// //     scrollTrigger: {
+// //         trigger: ".y1",
+// //         toggleActions: "restart none none none"
+// //     },
+// //     x: 400,
+// //     rotation: 360,
+// //     duration: 3
+// // })
+
+
+// gsap.to('.y1', {
+//     duration:2.5,
+// ease: "power4.out",
+// x: "400%"
+//     });
